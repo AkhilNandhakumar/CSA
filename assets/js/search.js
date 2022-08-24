0f91da4e7ea2c193a4d47fd1fbeb165b57e50792
AkhilNandhakumar <AkhilNandhakumar@users.noreply.github.com>
2022-08-24 18:05:02 +0000

    deploy: 3a7c62b342b0404309a93dd88bdc2b391e89b6d8

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/CSA/assets/js/search-data.json', true);
+    request.open('GET', '/CSA-Blog/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
